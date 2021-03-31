4ed52e893053e7b166cbd9460a7386793e42a0d6
kusasen <kusasen@gmail.com>
2021-03-31 17:31:25 +0800

    update

---
+++ dist/script.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.options = void 0;
+var options = {
+  ext: {
+    loadimpact: {
+      projectID: 3531597,
+      // Test runs with the same name groups test runs together
+      name: "YOUR TEST NAME"
+    }
+  }
+};
+exports.options = options;
