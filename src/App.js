468a4ab13a6d98262e9858e7891c61180a5e8f4a
kusasen <kusasen@gmail.com>
2021-04-21 15:49:08 +0800

    update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import HomePage from "./component/index/HomePage";
 import TradShow from "./component/TradShow";
 import DownloadPdf from "./component/DownloadPdf";
 import "./assets/css/getac.css";
-import { BrowserRouter, Link, Route, Switch, Redirect } from "react-router-dom";
+import { HashRouter, Link, Route, Switch, Redirect } from "react-router-dom";
 
 import { useReducer } from "react";
 import i18n from "./i18n";
 import history from "./history";
 
-const URL = process.env.PUBLIC_URL;
+const URL = "/";
 
 function langRedirect(props) {
   const defaultLang = i18n.language;
@@ -25,17 +25,17 @@ function App() {
     <div className="App">
       <Header />
       <main>
-        <BrowserRouter history={history} basename={URL}>
+        <HashRouter history={history}>
           <Switch>
-            <Route exact path={URL + "/:lang"}>
+            <Route exact path="/:lang/">
               <HomePage />
               <Footer />
             </Route>
-            <Route path={URL + "/:lang/tradshow"} component={TradShow} />
-            <Route path={URL + "/:lang/downlaod"} component={DownloadPdf} />
+            <Route path="/:lang/tradshow" component={TradShow} />
+            <Route path="/:lang/downlaod" component={DownloadPdf} />
             <Route path="*" render={langRedirect} />
           </Switch>
-        </BrowserRouter>
+        </HashRouter>
       </main>
     </div>
   );
